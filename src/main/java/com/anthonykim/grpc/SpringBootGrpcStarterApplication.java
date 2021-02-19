aa451eebbc30237999e79edf5184fd72fb78d039
Anthony Kim <anthony.jinhyuk.kim@gmail.com>
2021-02-19 11:25:33 +0900

    Initial commit

---
+++ src/main/java/com/anthonykim/grpc/SpringBootGrpcStarterApplication.java
@@ -0,0 +1,13 @@
+package com.anthonykim.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGrpcStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGrpcStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anthonykim/grpc/SpringBootGrpcStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonykim.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrpcStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
